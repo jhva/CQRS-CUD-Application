51b9ebf9e9008c8a1990aef4a2734d6a0b7ea730
jhva <junghun8158@naver.com>
2023-05-04 16:09:02 +0900

    :sparkles: 카프카 컨트롤러 추가

---
+++ src/main/java/com/example/kafkaspringboot/controller/KafkaController.java
@@ -0,0 +1,33 @@
+package com.example.kafkaspringboot.controller;
+
+import com.example.kafkaspringboot.config.KafkaProducer;
+import com.example.kafkaspringboot.dto.RequestDtoKid;
+import com.example.kafkaspringboot.dto.ResponseMan;
+import com.example.kafkaspringboot.kafkaService.ProducerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kafkaspringboot.repository.ManRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class KafkaController {
+    private final ManRepository manRepository;
+
+    private final ProducerService producerService;
+
+    @PostMapping("/man/save")
+    public ResponseEntity saveMan(@RequestBody RequestDtoKid dto) {
+        // return ResponseEntity.ok()
+        ResponseMan responseMan = producerService.sendMessage(dto);
+        return ResponseEntity.ok().body(responseMan);
+    }
+}
